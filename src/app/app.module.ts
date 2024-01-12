a3381eae559cbe6bab98ce47f1e1f1439eff5119
vivekjadhav7859 <vivek.jadhav.edu@gmail.com>
2024-01-12 14:51:22 +0530

    added navbar and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HerosectionComponent } from './components/herosection/herosection.component';
+import { AboutComponent } from './components/about/about.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ConnectComponent } from './components/connect/connect.component';
+import { CreateComponent } from './components/create/create.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const appRoute: Routes = [
+  { path: "", component: HerosectionComponent },
+  { path: "about", component: AboutComponent },
+  { path: "create", component: CreateComponent },
+  { path: "connect", component: ConnectComponent },
+  { path: "signup", component: SignupComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HerosectionComponent,
+    AboutComponent,
+    SignupComponent,
+    ConnectComponent,
+    CreateComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, MatToolbarModule, MatButtonModule, MatIconModule,
+    RouterModule.forRoot(appRoute), MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
